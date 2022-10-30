adb8f9c35ab15e160d4858d40078bddc3720f943
Raspberry Wolf <108033341+GuinnessShep@users.noreply.github.com>
2022-10-30 13:45:46 +0000

    condense

--- README.md
+++
@@ -1,8 +0,0 @@
-# Coding-project
-
-Rickroll your friends with some nodejs.
-
-It seems like a normal project, just share the website link [here](https://coding-project.kindling4204.repl.co)
-with people, and **BOOM**, they've been rickrolled.
-
-![GPL-3.0-or-later](./img/gplv3withtext.png)
--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-/*
- * coding-project, a program to rickroll people
- * Copyright © 2021 Kindling4204 
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-// Requires
-const express = require('express');
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-// Set up server
-const app = express();
-var httpServer = http.createServer(app);
-
-// Get / handler
-app.get('/', (req, res) => {
-    res.sendFile('/home/runner/coding-project/rickroll.mp4'); // send the rickroll file
-});
-
-httpServer.listen(8080); // Listen on port 8080
+http.createServer(function (req, res) {
+  var file = path.join(__dirname, 'heman.mp4');
+  var stat = fs.statSync(file);
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4',
+    'Content-Length': stat.size
+  });
+  var readStream = fs.createReadStream(file);
+  readStream.pipe(res);
+}).listen(8080);
